b1be457b0f045709217e47c5b5e524d4d0fbbeeb
susu10-10 <successchukwu20@gmail.com>
2022-07-19 16:59:22 +0100

    nt

--- flaskr/flaskblog.py
+++ flaskr/flaskblog.py
@@ -10,12 +10,13 @@ app.config['SECRET_KEY'] = 'dev'
 def home():
     return render_template('index.html')
 
-@app.route('/register')
+@app.route('/register', methods=['GET','POST'])
 def register():
     form = RegisterForm()
+    if form.validate_on_submit()
     return render_template('register.html', form=form)
 
-@app.route('/login')
+@app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
     return render_template('login.html', form=form)
