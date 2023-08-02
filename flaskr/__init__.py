676e0ddec1701f8e6cd5ef8534d18ccf59efde4a
SuSu <75139663+susu10-10@users.noreply.github.com>
2023-08-02 18:26:31 +0100

    Update __init__.py

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -3,32 +3,22 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
-
 # configurations
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dev'
-#specify a url for the database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 # create a database instance
 db = SQLAlchemy(app)
 
-#initalize the Bcrypt
-bcrypt = Bcrypt(app) 
-# i didnt use this one again i made use of werzeug.security
-# has it has generated and check password hash
+# initialize the Bcrypt
+bcrypt = Bcrypt(app)
 
-#instance of login
+# instance of login
 login_manager = LoginManager(app)
 login_manager.login_view = 'login'
 login_manager.login_message_category = 'info'
 
 # importing the routes
 from flaskr import routes
-
-
-
-#
-#db.create_all()
-
-app.run()
